932918fac78d76e55ee30c2efbbf0f59c1a77bca
marcus210 <marcus_ultimate@hotmail.com>
2015-05-29 00:14:14 -0300

    Corrigido boa parte dos bugs, falta pouco...

--- TrabalhoSO2015/MVC/ArquivoDAO.cs
+++ TrabalhoSO2015/MVC/ArquivoDAO.cs
@@ -12,6 +12,8 @@ namespace TrabalhoSO2015
         int[] prateleira;
         ArquivoDAL arquivoDAL = new ArquivoDAL();
         Relatorio relatorioDAL;
+        int posV = 0;
+        int cont = 0;
 
         
 
@@ -22,7 +24,7 @@ namespace TrabalhoSO2015
 
             string texto = "";
             string txtLine = null;
-            int cont = 0;
+            
             do
             {
                 txtLine = strR.ReadLine();               
@@ -30,52 +32,67 @@ namespace TrabalhoSO2015
                 texto += txtLine;
                 cont++;
             }
-            while (txtLine != null);           
+            while (txtLine != null);          
 
             return relatorioDAL;
         }
 
         public void Fifo(string linha, int pos)
         {
-            int posV = 0;
-            bool existe = false;
+            
+            
 
-            if(pos == 0)
-                prateleira = new int [int.Parse(linha)];
+            if (pos == 0)
+            {
+                prateleira = new int[int.Parse(linha)];
+                //relatorioDAL.Falta = int.Parse(linha);
+            }
             else
-                if(linha != null)
-                foreach(char c in linha.ToCharArray())
-                {
-                    if (prateleira != null && Program.debug == true)
-                        uiFila(pos,posV);
-                    if(c != char.Parse("."))
+                if (linha != null)
+                    foreach (char c in linha.ToCharArray())
                     {
-                        for (int z = 0; z < prateleira.Length; z++)//Percorre todo o vetor prateleira e verifica se o valor ja existe
+                        bool existe = false;
+                        int valorAtual;
+                        if (prateleira != null && Program.debug == true)
+                            uiFila(pos, posV);
+                        
+
+                        if (c != char.Parse("."))
                         {
-                            if (prateleira[z] == c)
+                            valorAtual = int.Parse(c.ToString());
+
+
+                            for (int z = 0; z < prateleira.Length; z++)//Percorre todo o vetor prateleira e verifica se o valor ja existe
                             {
-                                existe = true;
+                                if (prateleira[z] == int.Parse(c.ToString()))
+                                {
+                                    existe = true;
+                                }
+                            }
+
+                            
+                            if (existe == false)
+                            {
+                                //if (prateleira[posV] != null)
+                                // relatorioDAL.Substituido[posV]++;
+
+                                prateleira[posV] = valorAtual;
+                                relatorioDAL.Produtos[valorAtual]++;
+
+                                if (posV + 1 == prateleira.Length)
+                                {
+                                    posV = 0;
+                                    continue;
+                                }
+                                relatorioDAL.Falta++;
+                                posV++;
+                            }
+                            else
+                            {
+                                relatorioDAL.Produtos[valorAtual]++;                               
                             }
                         }
-
-                        if (existe == false)
-                        {
-                            if (prateleira[posV] != null)
-                                relatorioDAL.Substituido[posV]++;
-
-                            prateleira[posV] = int.Parse(c.ToString());
-                            relatorioDAL.Produtos[posV]++;
-
-                            if (posV + 1 == prateleira.Length)
-                            {
-                                posV = 0;
-                                continue;
-                            }
-                            relatorioDAL.Falta++;
-                            posV++;
-                        }
-                    }                    
-                }
+                    }
             
         }
 
@@ -85,7 +102,7 @@ namespace TrabalhoSO2015
             string visualFila = "";
             for(int i = 0; i < prateleira.Length; i++)
             {                
-                visualFila += "[ "+arquivoDAL.Produto[i]+" ] ";
+                visualFila += "[ "+prateleira[i]+" ] ";
             }
             Console.WriteLine("Linha: " +pos+ " ciclo " + (posV + 1) + ": " + visualFila);
         }
